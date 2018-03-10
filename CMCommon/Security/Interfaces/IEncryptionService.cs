2e904ba780fb90a34d35e5f255038c73fc424ef4
Cyberminds Admin <admin@cyberminds.co.uk>
2018-03-10 21:21:46 +0000

    INitial commit.

---
+++ CMCommon/Security/Interfaces/IEncryptionService.cs
@@ -0,0 +1,16 @@
+﻿
+namespace CMCommon.Security.Interfaces
+{
+    public interface IEncryptionService
+    {
+        string Encrypt(string clearText, string Password);
+
+        byte[] Encrypt(byte[] clearData, byte[] Key, byte[] IV);
+
+        string Decrypt(string str);
+
+        string Decrypt(string cipherText, string Password);
+
+        byte[] Decrypt(byte[] cipherData, byte[] Key, byte[] IV);
+    }
+}
